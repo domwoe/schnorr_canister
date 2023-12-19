f4ab2ed98034e540f7ff9dce78bf9362d2d3a7fd
Dominic Woerner <dominic.woerner@dfinity.org>
2023-12-19 20:47:11 +0100

    simplify project structure
    Signed-off-by: Dominic Woerner <dominic.woerner@dfinity.org>

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cargo build --release --target wasm32-unknown-unknown --package schnorr_canister_backend
-candid-extractor target/wasm32-unknown-unknown/release/schnorr_canister_backend.wasm > src/schnorr_canister_backend/schnorr_canister_backend.did
+cargo build --release --target wasm32-unknown-unknown --package schnorr_canister
+candid-extractor target/wasm32-unknown-unknown/release/schnorr_canister.wasm > schnorr_canister.did
 dfx deploy
